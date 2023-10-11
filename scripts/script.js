88ffdfe92d74f3dd19daba9af7f86174a0347772
Maria <zhukovamaria9@gmail.com>
2023-10-11 22:08:03 +0300

    create gallery

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let imgLake = document.getElementById("image-lake");
+
+function changePicPrevious() {
+  imgLake.style.backgroundImage = 'url("../assets/images/photo2.jpg")';
+}
+
+let imgFlower = document.getElementById("image-lake");
+
+function changePicNext() {
+  imgFlower.style.backgroundImage = 'url("../assets/images/photo1.jpg")';
+}
